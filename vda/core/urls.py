2bdfe11fe866b3f8983a3b37c5a1f7c31cafa853
lambrou <alexanderlambrou0602@gmail.com>
2020-08-24 13:02:34 -0700

    Adjusted CSS styles and added useful information to the side navigation

--- vda/core/urls.py
+++ vda/core/urls.py
@@ -1,9 +1,15 @@
+from django.contrib.auth import views as auth_views
+
 from django.urls import include, path
 from . import views
+import configparser
+config = configparser.ConfigParser()
+config.read('core/static/core/navinfo.ini')
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('users/', include('django.contrib.auth.urls')),
+    path('users/login', auth_views.LoginView.as_view(extra_context = {'navinfo': config['DEFAULT']['BruteForce']})),
     path('users/changepass', views.changepass, name='changepass'),
     path('users/signup', views.signup, name='signup'),
     path('users/profile', views.profile, name='signup'),
--- vda/core/views.py
+++ vda/core/views.py
@@ -4,16 +4,20 @@ from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 import subprocess
 from django.views.decorators.csrf import csrf_exempt
-from django.templatetags.static import static
 from django.contrib.auth.models import User
 from core.models import Guestbook
 from django.utils import timezone
+import configparser
 
+config = configparser.ConfigParser()
+config.read('core/static/core/navinfo.ini')
 
 def index(request):
-    return render(request, 'core/index.html', None)
+    context = { "navinfo": config['DEFAULT']['Index'] }
+    return render(request, 'core/index.html', context)
 
 def signup(request):
+
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -25,30 +29,39 @@ def signup(request):
             return redirect('/')
     else:
         form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+    return render(request, 'registration/signup.html')
 
 def profile(request):
     return render(request, 'user/profile.html', None)
 
 def testconn(request):
+    context = { "navinfo": config['DEFAULT']['CommandExec'] }
     if request.method == 'POST':
         webAddr = request.POST.get('serveraddr')
         procOut = subprocess.check_output('powershell.exe Test-Connection ' + webAddr, shell=True)
-        context = { "stdout": procOut.decode() }
+        context = { 
+            "stdout": procOut.decode(),
+            "navinfo": config['DEFAULT']['CommandExec']
+        }
         return render(request, 'utils/testconn.html', context)
-    return render(request, 'utils/testconn.html', None)
+    return render(request, 'utils/testconn.html', context)
 
 @csrf_exempt
 def changepass(request):
+    context = { "navinfo": config['DEFAULT']['CSRF'] }
     if request.method == 'POST':
         password1 = request.POST.get('newpass1')
         password2 = request.POST.get('newpass2')
         if (password1 == password2):
-            context = { "msg": 'Your new password is ' + password1 }
+            context = { "msg": 'Your new password is ' + password1,
+                        "navinfo": config['DEFAULT']['CSRF']
+            }
         else:
-            context = { "msg": 'Passwords did not match.' }
+            context = { "msg": 'Passwords did not match.',
+                        "navinfo": config['DEFAULT']['CSRF']
+             }
         return render(request, 'user/changepass.html', context)
-    return render(request, 'user/changepass.html', None)
+    return render(request, 'user/changepass.html', context)
 
 def filerunner(request):
     if request.method == 'GET':
@@ -59,23 +72,28 @@ def filerunner(request):
                 "stdout": procOut.decode(),
                 "file1": "dt.py",
                 "file2": "version.py",
-                "file3": "fibonacci.py"
+                "file3": "fibonacci.py",
+                "navinfo": config['DEFAULT']['FileInclusion']
             }
             return render(request, 'utils/filerunner.html', context)
     context = {
         "file1": "dt.py",
         "file2": "version.py",
-        "file3": "fibonacci.py"
+        "file3": "fibonacci.py",
+        "navinfo": config['DEFAULT']['FileInclusion']
     }
     return render(request, 'utils/filerunner.html', context)
 
 def userlookup(request):
+    context = { "navinfo": config['DEFAULT']['SQLi'] }
     if request.method == 'POST':
         uname = request.POST.get('uname')
         query = User.objects.raw('SELECT * from "auth_user" WHERE "auth_user"."username" = "' + uname + '"')
-        context = { "stdout": query }
+        context = { "stdout": query,
+                    "navinfo": config['DEFAULT']['SQLi']
+        }
         return render(request, 'utils/userlookup.html', context)
-    return render(request, 'utils/userlookup.html', None)
+    return render(request, 'utils/userlookup.html', context)
 
 def guestbook(request):
     
@@ -86,7 +104,9 @@ def guestbook(request):
         gbook.save()
     posts = Guestbook.objects.all()
     if posts:
-        context = { "posts": posts }
+        context = { "posts": posts,
+                    "navinfo": config['DEFAULT']['XSSs']
+        }
     else:
-        context = { None: None }
+        context = { "navinfo": config['DEFAULT']['XSSs'] }
     return render(request, 'utils/guestbook.html', context)
